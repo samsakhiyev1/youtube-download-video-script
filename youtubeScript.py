bbcbcf1d5284ad70d9c3011b1936c9a9b5f9d444
Sam <72391438+samsakhiyev1@users.noreply.github.com>
2021-03-01 23:08:09 +0200

    Update youtubeScript.py

--- youtubeScript.py
+++ youtubeScript.py
@@ -2,4 +2,4 @@ from pytube import YouTube
 url = input("Enter the youtube link: ")
 video = YouTube(url)
 stream = video.streams.get_highest_resolution()
-stream.download('/home/sam/Downloads')
+stream.download('path/to/folder')
